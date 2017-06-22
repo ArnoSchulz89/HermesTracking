71798def24ab9b01c697b7353e8320d7afb7b2d8
Arno Schulz <arno.schulz89@gmail.com>
2017-06-22 14:01:49 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -14,13 +14,16 @@ restService.post('/hermestracking', function(req, res) {
     var googleReq = req.body.result.parameters.barcode.replace(/[^0-9.]/g, "");
     //console.log(googleReq);
 
-    var hermesRes = tracking.getTracking(googleReq, function (error, response, body) {
+    // getTracking(barcode-String, get-Method-to-hermes-tracking-API);
+    // hermesRes --> result String from Hermes-API
+    var trackingCall = tracking.getTracking(googleReq, function (error, response, body) {
         if (!error && response.statusCode == 200) {
             var latest = JSON.parse(body)[0];
             var hermesRes = answers.buildAnswerSentence(latest);
 
+            //returns the json-response(res) to google. 
             if(hermesRes != ''){
-                var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.barcode ? hermesRes : "I didn't get it rigth, please Speak again."
+                var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.barcode ? hermesRes : "I didn't get it right, please Speak again."
                 return res.json({speech: speech, displayText: speech, source: 'gHomeHermesTrackingAPI'});
             }
         }else{
